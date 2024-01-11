d35ff2f17053b71b7919a3dad3f055bea419107a
lindaislinda <thaonguyenntp2003@gmail.com>
2024-01-10 21:25:09 -0500

    Style QnA

--- src/components/FAQ/QA.jsx
+++ src/components/FAQ/QA.jsx
@@ -4,19 +4,19 @@ import { useState } from 'react';
 export function QA({question, answer}) {
   const [expand, setExpand] = useState(false);
   const expandClass = expand ? 'display' : 'hidden';
-  const borderRadius = expand ? 'rounded-3xl' : 'rounded-t-full rounded-b-full'
-  const ansClass = `${expandClass} p-4 text-left text`
-  const container = `${borderRadius} shadow rounded-3xl border border-gray-100`
+  const borderRadius = expand ? 'rounded-3xl' : 'rounded-3xl'
+  const ansClass = `${expandClass} century-ps about-us-font-color text-xl text-left text px-4 py-4`
+  const container = `${borderRadius} shadow rounded-1xl border border-gray-100`
   
   return (
     <div className={container}>
-      <div className="p-4 text-xl relative font-medium flex ">
-        <div className="century-ps about-us-font-color text-xl text-left w-5/6 flex-auto">
+      <div className="text-xl relative font-medium flex ">
+        <div className="century-ps about-us-font-color text-xl text-left w-5/6 flex-auto px-4 py-4">
           {question}
         </div>
         <button
           aria-label="question-expander"
-          className="text-xl p-4 relative bg-transparent border-none flex-auto faq-btn text"
+          className="text-xl p-4 relative bg-transparent border-none about-us-font-color flex-auto faq-btn text"
           onClick={() => setExpand(!expand)}
         >
           {
