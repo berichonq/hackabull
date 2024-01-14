eeaaa1e1edc749ef8c463ec624a7f01a234b933d
lindaislinda <thaonguyenntp2003@gmail.com>
2024-01-14 08:37:14 -0800

    Complete styling login page

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,48 +1,83 @@
-import s from './style.module.css'
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { logOn } from '../../store/user/user-slice'
+import "./LoginForm.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logOn } from "../../store/user/user-slice";
 
-import { auth, usersCollectionRef } from '../../config/firebase'
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { doc, getDoc } from 'firebase/firestore'
-
-
+import { auth, usersCollectionRef } from "../../config/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+const textColor = {
+    color: "#052B56",
+    transition: "color 0.3s ease", // Smooth color transition on hover
+  };
 export function LoginForm() {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [validate, setValidated] = useState()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validate, setValidated] = useState();
 
-    let dispatch = useDispatch()
+  let dispatch = useDispatch();
 
-    const login = async () => {
-        try {
-            await signInWithEmailAndPassword(auth, email, password)
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
 
-            ///////////////////////////////////////////////////////////////////////////////
-            // Access the logged in user's data from Firestore and update the Redux state
-            const docRef = doc(usersCollectionRef, auth?.currentUser?.email)
-            let user;
-            try {
-                user = await getDoc(docRef)
-            } catch(err) {
-                console.error(err)
-            }
+      ///////////////////////////////////////////////////////////////////////////////
+      // Access the logged in user's data from Firestore and update the Redux state
+      const docRef = doc(usersCollectionRef, auth?.currentUser?.email);
+      let user;
+      try {
+        user = await getDoc(docRef);
+      } catch (err) {
+        console.error(err);
+      }
 
-            dispatch(logOn(user.data()))
-            //////////////////////////////////////////////////////////////////////////////
-        } catch (err) {
-            alert(err)
-        }
+      dispatch(logOn(user.data()));
+      //////////////////////////////////////////////////////////////////////////////
+    } catch (err) {
+      alert(err);
     }
+  };
 
-    return (
-        <div>
-            <input placeholder="Email" id="email" autoComplete="email" type="email" onChange={(e) => setEmail(e.target.value)}/>
-            <input placeholder="Password" name="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
-            <br/>
-            <button onClick={login}>Login</button>
-            <br/>
-        </div>
-    )
+  return (
+    <div>
+      <div className="form-line">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-900 dark:text-white"
+          style={textColor}
+        >
+          Email
+        </label>
+        <input
+          required
+          id="email"
+          autoComplete="email"
+          placeholder="Email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-white border border-gray-800 rounded-md p-2 login-input"
+        />
+      </div>
+      <div className="form-line">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-900 dark:text-white"
+          style={textColor}
+        >
+          Password
+        </label>
+        <input
+          required
+          id="password"
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-white border border-gray-800 rounded-md p-2 login-input"
+        />
+      </div>
+      <br />
+      <button onClick={login} className="login-button">Login</button>
+      <br />
+    </div>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,51 @@
-import s from './style.module.css'
-import { Link } from 'react-router-dom'
+import "./Login.css";
+import loginImage from "../../assets/images/login-page.png";
 
-import { LoginForm} from '../../components/LoginForm/LoginForm'
+import { Link } from "react-router-dom";
+
+import { LoginForm } from "../../components/LoginForm/LoginForm";
+
+const linkStyle = {
+  color: "#052B56",
+  transition: "color 0.3s ease", // Smooth color transition on hover
+};
 
 export function Login() {
-    
-    return (
-        <div>
-            Welcome to login
-            <br/>
+  return (
+    <div className="century-ps text-xl text-left h-screen">
+      <div className="row">
+        <div className="col-lg col-md">
+          <div className="center">
+            <p
+              className="title text-2xl bold-century"
+              style={linkStyle}
+              id="title"
+            >
+              Welcome to Hackabull 2024 at USF!
+            </p>
+            <br />
             <LoginForm />
-            Haven't registered yet? <Link to='/register'>Register here</Link>
-            <br/>
-            Forgot your password? <Link to='/password-reset'>Click here</Link>
-            <br/>
-            Experiencing issues? <a href="mailto:shpe.hackabull@gmail.com?subject=Account%20support%20request">Contact us</a>
+            <Link to="/register" className="linkStyle">
+              Haven't registered?
+            </Link>
+            <br />
+            <Link to="/password-reset" className="linkStyle">
+              Forgot password?{" "}
+            </Link>
+
+            <br />
+            <a
+              href="mailto:shpe.hackabull@gmail.com?subject=Account%20support%20request"
+              className="linkStyle"
+            >
+              Experiencing issues?{" "}
+            </a>
+          </div>
         </div>
-    )
+        <div className="col-lg col-md d-none d-md-block">
+          <img className="login-img" src={loginImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
