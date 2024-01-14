8c716bc1f0b872b1797fbdcdc463c269b2a372fe
lindaislinda <thaonguyenntp2003@gmail.com>
2024-01-14 06:48:59 -0800

    initialize register form

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./style.module.css";
+
+function RegisterForm() {
+  return (
+    <div>RegisterForm</div>
+  )
+}
+
+export default RegisterForm
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import s from './style.module.css'
 import { RegistrationForm } from '../../components/RegistrationForm/RegistrationForm'
 
@@ -9,3 +10,19 @@ export function Register() {
         </div>
     )
 }
+=======
+import s from "./style.module.css";
+import { Link } from "react-router-dom";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+export function Register() {
+  return (
+    <div>
+      <RegisterForm />
+      Welcome to registration
+      <br />
+      Already registered? <Link to="/login">Login here</Link>
+    </div>
+  );
+}
+>>>>>>> Stashed changes
