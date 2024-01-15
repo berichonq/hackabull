0d7c000cf7924df06c246f72944b3c2f4b734172
Quinn <quinnberichon@gmail.com>
2024-01-15 14:40:16 -0500

    merge fix

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,7 @@
 import "./Profile.css";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import profilePicture from "../../assets/images/profile-pic.png";
-import logo from "../../assets/images/LineArt-bold-blue.png";
+
 import { useDispatch, useSelector } from "react-redux";
 import { logOff } from "../../store/user/user-slice";
 
@@ -15,6 +14,7 @@ import {
 import { deleteDoc, doc, setDoc } from "firebase/firestore";
 
 import profilePicture from "../../assets/images/profile-pic.png";
+import logo from "../../assets/images/LineArt-bold-blue.png";
 
 export function Profile() {
   const user = useSelector((state) => state.user.data);
