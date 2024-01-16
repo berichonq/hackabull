05ee78f9d5e0461f0f2e96a3ede0db748c684839
Asadbek <asadbekn443@gmail.com>
2024-01-15 23:54:51 -0500

    loader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import "./Home.css";
+import React, { useState, useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { logOff } from "../../store/user/user-slice";
 import { auth } from "../../config/firebase";
 import { signOut } from "firebase/auth";
-import { AboutUs } from "../../components/AboutUs/aboutus";
+import { AboutUs } from "../../components/AboutUs/AboutUs";
 import { PastEvents } from "../../components/PastEvents/PastEvents";
 import { FAQ } from "../../components/FAQ/FAQ";
 import { Schedule } from "../../components/Schedule/Schedule";
@@ -15,8 +16,15 @@ import { HomeSection } from "../../components/HomeSection/HomeSection";
 import { Footer } from "../../components/Footer/Footer";
 
 export function Home() {
+    useEffect(() => {
+        // Simulate a loading process, e.g., fetching data
+        setTimeout(() => setIsLoading(false), 2000); // remove timeout in real use case
+    }, []);
+
     //////////////////////////////////////////////////////////////////////////////////////////////
     // Logout button needs to be moved to navbar. Should only be rendered if user is authenticated
+    const [isLoading, setIsLoading] = useState(true);
+
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -33,30 +41,42 @@ export function Home() {
 
     return (
         <div className="home">
-            <div id="home">
-                <HomeSection />
-            </div>
-            <div className="content">
-                <div id="about">
-                    <AboutUs />
+            {isLoading ? (
+                <div className="loading-screen">
+                    <div className="loader"></div>{" "}
                 </div>
-                <div id="events">
-                    <PastEvents />
-                </div>
-                <div id="schedule">
-                    <Schedule />
-                </div>
-                <div id="sponsors">
-                    <Sponsors />
-                </div>
-                <div id="faq">
-                    <FAQ />
-                </div>
-                <div id="team">
-                    <Team />
-                </div>
-                <Footer />
-            </div>
+            ) : (
+                <>
+                    <div id="home">
+                        <HomeSection />
+                    </div>
+                    <div className="content">
+                        <div className="content">
+                            <div id="about">
+                                <AboutUs />
+                            </div>
+                            <div id="events">
+                                <PastEvents />
+                            </div>
+                            <div id="schedule">
+                                <Schedule />
+                            </div>
+                            <div id="sponsors">
+                                <Sponsors />
+                            </div>
+                            <div id="faq">
+                                <FAQ />
+                            </div>
+                            <div id="team">
+                                <Team />
+                            </div>
+                            <Footer />
+                        </div>
+                        //{" "}
+                    </div>
+                    //{" "}
+                </>
+            )}
         </div>
     );
 }
