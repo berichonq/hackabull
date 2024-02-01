8cba06fef2a7e721d571150f57d18c42cc26aa56
Asadbek <asadbekn443@gmail.com>
2024-02-01 13:43:06 -0500

    logo

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -44,7 +44,7 @@ export function Sponsors() {
                             <img
                                 src={secondImage}
                                 alt="Brand Logo 2"
-                                className="w-auto h-30"
+                                className="w-auto h-28"
                             />
                         </a>
                     </div>
